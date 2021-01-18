f8dd151724288febb746a77f1690d4f8bd3af3fe
anmutu <anmutu@hotmail.com>
2021-01-18 23:34:08 +0800

    03longest-substring-without-repeating-characters

---
+++ arithmetic/02array/14-03longest-substring-without-repeating-characters/14-03longest-substring-without-repeating-characters.go
@@ -0,0 +1,17 @@
+/*
+  author='du'
+  date='2021/1/18 23:28'
+  github='https://github.com/anmutu'
+*/
+package main
+
+import "fmt"
+
+func main() {
+	s := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	return 0
+}
