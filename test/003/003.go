9c1516200941f0fa96b561abcaadf4011c1fbd7d
guanmai <anmutu@hotmail.com>
2021-01-21 10:51:41 +0800

    leetcode03

--- test/003/003.go
+++ test/003/003.go
@@ -9,32 +9,42 @@ import (
 	"math"
 	"strings"
 )
-//abcabcbb
-//a   //left不动，right++
-//ab
-//abc
-// bca 移除窗口中的第一个元素，left++,right++
 
+//abcabcbb
+//a   //left不动，right++   =>ab
+//ab  //left不动，right++   =>abc
+//abc                      =>abca
+//abca //移除窗口中的第一个元素，left++,right++     =>bca  =>纪录3
+// bca
 func main() {
 	//var s="abcabcbb"
 	//fmt.Println(lengthOfLongestSubstring(s))
-	s1:="abcbabc"
+	s1 := "abcbabc"
 	fmt.Println(lengthOfLongestSubstring(s1))
 }
 
+/*
+ 定义一个左右指针，left和right
+ 使可得到窗口window[left:right]
+ 判断windows中是否有s[right]元素
+ 如果存在:
+ 则需要将left的位置变更为当前下标位置+1，
+ 且right也+1,
+ 取此时的window，判断其大小是不是每次最大的。
+ 如果不存在：
+ 则需要将right往后移一步。
+*/
+
 func lengthOfLongestSubstring(s string) int {
-	left,right:=0,0
-	window:=s[left:right]
-	res:=0
-	for ;right<len(s);right++{
-		if index:=strings.IndexByte(window,s[right]);index!=-1{//表示存在
-			left+=left+1
+	left, right := 0, 0
+	window := s[left:right]
+	res := 0
+	for ; right < len(s); right++ {
+		if index := strings.IndexByte(window, s[right]); index != -1 { //表示存在
+			left += index + 1
 		}
-		window=s[left:right+1]
-		//if len(window)>res{
-		//	res=len(window)
-		//} 
-		res=int(math.Max(float64(res),float64(len(window))))
+		window = s[left : right+1]
+		res = int(math.Max(float64(res), float64(len(window))))
 	}
 	return res
 }
