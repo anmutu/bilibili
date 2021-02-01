ea4b28a86ef56ed438e6124e3390801facd84934
guanmai <anmutu@hotmail.com>
2021-02-01 13:21:25 +0800

    leetcode075题目描述。

---
+++ arithmetic/02array/16-076-minimum-window-substring/16-075minimum-window-substring.go
@@ -0,0 +1,20 @@
+/*
+ author='du'
+ date=2021/2/1 1:15 下午
+ github='https://github.com/anmutu'
+ sourcecode='https://github.com/anmutu/bilibili'
+*/
+
+package main
+
+//链接：https://leetcode-cn.com/problems/minimum-window-substring
+//给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。
+//如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+//注意：如果 s 中存在这样的子串，我们保证它是唯一的答案。
+//示例 1：
+//输入：s = "ADOBECODEBANC", t = "ABC"
+//输出："BANC"
+
+func main() {
+
+}
